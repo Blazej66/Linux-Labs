cef1f0f26aee4a9a110bc94f772d7881fb4d8a5b
Blazej_Kie <123500381+Blazej66@users.noreply.github.com>
2026-04-07 01:33:36 +0200

    checking chmod etc with automation script

---
+++ lab_four.sh
@@ -0,0 +1,37 @@
+
+#!/bin/bash
+# Task 1 
+echo "Task 1"
+pwd
+# Task 2 
+echo "Task 2"
+>> app
+chmod u+x app
+# Task 3 
+echo "Task 3"
+touch prog1.c prog2.c test.c
+# Task 4 
+echo "Task 4"
+echo File number one > tekst1.txt
+echo File bon vile > tekst2.txt
+# Task 5 
+echo "Task 5"
+ls t*
+# Task 6
+echo "Task 6"
+mkdir -p applications/documentation
+# Task 7
+echo "Task 7"
+cp /home/student/*.txt /home/student/applications/documentation/
+# Task 8 
+echo "Task 8"
+ mv /home/student/*.c /home/student/applications/
+# Task 9
+echo "Task 9"
+rm ./b*
+# Task 10
+echo "Task 10"
+find /usr/ -name "program" | grep -r "Found xxx" .
+# Task 11 
+echo "Task 11"
+ln -s /home/student/teskt1.txt /home/student/applications/documentation/tekst1-link.txt 
