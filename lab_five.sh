fce6a2693a5127c6742527fc1763bfac9a32aa9d
Blazej_Kie <123500381+Blazej66@users.noreply.github.com>
2026-04-07 01:35:36 +0200

    Creating automation task #3

---
+++ lab_five.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Blazej Kiermacz"
+# Task 2 
+echo "Solution for Task 2:ls -l /home/student/ | sort -bnr +4 -5 | head"
+# Task 3 
+echo "Solution for Task 3:ls -l /etc/ | sort -bnr +4 -5 | tail -6"
+# Task 4 
+echo "Solution for Task 4:/tmp$ grep -rni /tmp/"
+# Task 5 
+echo "Solution for Task 5:find /usr/ -name jvm* > unavailable.txt 2>&1"
+
